ef6bb51484ee6220a6beaa67b1065a644bf63634
fthiagomedeiros <fthiagomedeiros@gmail.com>
2023-12-15 00:29:23 +0000

    Setting up properties with jinja2

--- src/main/java/com/spring/ansible/BookController.java
+++ src/main/java/com/spring/ansible/BookController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.yml")
 @RestController
 @RequestMapping("/api/hello")
 public class BookController {
