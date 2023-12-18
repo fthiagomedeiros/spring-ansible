9d0ba1cb0bea3f41ae6acc49f00f91616af58712
Thiago Medeiros <fthiagomedeiros@gmail.com>
2023-12-18 21:50:07 +0000

    properties file

--- src/test/java/com/spring/ansible/SpringAnsibleApplicationTests.java
+++ src/test/java/com/spring/ansible/SpringAnsibleApplicationTests.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 @WebMvcTest(value = BookController.class)
